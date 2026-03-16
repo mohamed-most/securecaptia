9a44efee6e3fa93b910c47c842bcd963881b312a
mohamed mostafa <mohmost25@gmail.com>
2026-03-16 23:54:34 +0200

    feat: initial setup for forgot password functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/login/login';
+import { ForgotPassword } from './features/forgot-password/forgot-password';
 export const routes: Routes = [
   {
     path: 'login',
     component: Login,
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPassword,
   },
   {
     path: '',
